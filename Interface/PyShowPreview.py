97762fe1d210d745abae4cf0d74b85fc788e3243
Raimond Frentrop <raimund89@gmail.com>
2018-06-13 20:22:00 +0200

    First try at the drawing. Many, many mistakes and unsolved issues..

--- Interface/PyShowPreview.py
+++ Interface/PyShowPreview.py
@@ -17,8 +17,8 @@
 """
 
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtGui import QPainter, QColor, QLinearGradient
-from PyQt5.QtCore import QRect, Qt
+from PyQt5.QtGui import QPainter, QColor, QLinearGradient, QFont, QPen
+from PyQt5.QtCore import QRect, Qt, QPoint
 
 
 class PyShowPreview(QWidget):
@@ -94,6 +94,10 @@ class PyShowPreview(QWidget):
         # Finish drawing
         painter.end()
 
+    def refresh(self, data, cursor):
+        """Called when an update of the GUI is necessary"""
+        self._slide.refresh(data, cursor)
+
 
 class PyShowSlide(QWidget):
     """The actual slide inside the preview widget"""
@@ -105,6 +109,9 @@ class PyShowSlide(QWidget):
 
         self._size = (0, 0)
 
+        self._cursor = None
+        self._data = None
+
     def set_size(self, width, height):
         """Set the slide size in pixels"""
         self._size = (width, height)
@@ -113,6 +120,12 @@ class PyShowSlide(QWidget):
     def size(self):
         """Returns the slide size in pixels"""
         return self._size
+
+    def refresh(self, data, cursor):
+        """Refresh the preview with new parsed data or cursor position"""
+        self._data = data
+        self._cursor = cursor
+        self.update()
 
     def paintEvent(self, event):
         """Called when the slide preview needs to be updated"""
@@ -124,13 +137,95 @@ class PyShowSlide(QWidget):
 
         # Define slide area
         rect = self.geometry()
+        print(rect)
+        scaled = rect.width()/self._size[0]
+        print(scaled)
 
-        # Draw a rect for the background
-        painter.fillRect(QRect(0,
-                               0,
-                               rect.width(),
-                               rect.height()),
-                         QColor("#FFF"))
+        # We know the cursor position and the parsed data. We want to know
+        # which line corresponds to the cursor position, so we can find the
+        # last newSlide-statement and prepare the preview from there.
+        if self._data is not None and self._cursor is not None:
+            data = self._data[self._cursor[0]]["contents"]
+            i = 0
+            while (data[self._cursor[1]-i]["name"] != "newSlide" and
+                   self._cursor[1]-i > -1):
+                i += 1
+
+            if data[self._cursor[1]-i]["name"] != "newSlide":
+                print("ERROR: no newSlide in block!")
+                painter.end()
+                return
+
+            # Check if the template exists
+            template = None
+            for block in self._data:
+                print(block["name"])
+                print(block["args"])
+                if (block["name"] == "beginTemplate" and
+                    block["args"][0] == data[self._cursor[1]-i]["args"][0]):
+                    template = block["contents"]
+                    break
+
+            if template is None:
+                print("ERROR: no template with this name!")
+                painter.end()
+                return
+
+            # Run through the template and fill a dict with all standard
+            # objects. This list can be appended later by the script if
+            # new objects are created on the fly
+            objects = {}
+            for setting in template:
+                if setting["name"] == "setBackgroundColor":
+                    objects["background_color"] = QColor(setting["args"][0])
+                elif setting["name"] == "addTextBlock":
+                    objects["text_" + setting["args"][0]] = {}
+                    for entry in setting["args"][1:]:
+                        if len(entry) == 2:
+                            objects["text_" + setting["args"][0]][entry[0][0]] = entry[1]
+                        else:
+                            print("ERROR: value without a key!")
+
+            print(objects)
+
+            # The background
+            if objects["background_color"]:
+                color = objects["background_color"]
+            else:
+                color = QColor('#FFF')
+
+            # Draw a rect for the background
+            painter.fillRect(QRect(0,
+                                   0,
+                                   rect.width(),
+                                   rect.height()),
+                             color)
+
+            painter.fillRect(QRect(100*scaled,
+                                   100*scaled,
+                                   1700*scaled,
+                                   800*scaled),
+                             QColor('#00F'))
+
+            # Work through all the commands until the cursor
+            for j in range(i):
+                print(j+1)
+                command = data[self._cursor[1]-i+j+1]
+
+                if command["name"] == "setText":
+                    # Search the object list for this text object
+                    text = objects.get("text_" + command["args"][0])
+                    if text is None:
+                        print("ERROR: text object %s undefined" % (command["args"][0]))
+
+                    font = QFont(text["fontname"], int(text["fontsize"])*scaled)
+                    painter.setFont(font)
+                    if text["color"]:
+                        painter.setPen(QPen(QColor(text["color"])))
+                    painter.drawText(QPoint(int(text["x"])*scaled, int(text["y"]))*scaled,
+                                     command["args"][1])
+                else:
+                    print("WARNING: command '%s' unknown" % (command["name"]))
 
         # Finish drawing
         painter.end()
